613d51b116dd799ed0658a5208a861b4ef3a45b9
Caio Santana <caio.cad@gmail.com>
2026-02-15 23:08:15 -0300

    Fix: BrowserRouter no entrypoint (index.js)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import TrabalheiLa from './TrabalheiLa'; // ✅ Novo
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <TrabalheiLa />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
+
