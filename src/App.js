909980cc9cd98f195518416bfea2c1588316de8d
Caio Santana <caio.cad@gmail.com>
2026-02-17 12:23:47 -0300

    Debug: check LinkedIn env injection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./Home"; // ajuste se o seu "Home" tiver outro nome
-import CompanyPage from "./pages/CompanyPage";
+<div style={{position:"fixed", bottom:8, right:8, padding:"6px 10px", background:"#000", color:"#fff", fontSize:12, zIndex:99999}}>
+  DEPLOY_CHECK_17_02
+</div>
 
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/empresa/:slug" element={<CompanyPage />} />
-    </Routes>
-  );
-}
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
