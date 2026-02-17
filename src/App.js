b99191f0bd9029f2b99a3ace834127b58cc96980
Caio Santana <caio.cad@gmail.com>
2026-02-17 15:54:46 -0300

    Restore App.js (site back to working state)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+// importe suas páginas reais aqui
 
 export default function App() {
   return (
-    <div style={{ padding: 24 }}>
-      <h1>App carregou</h1>
-
-      {/* Debug temporário (pode remover depois) */}
-      <div
-        style={{
-          position: "fixed",
-          top: 8,
-          right: 8,
-          background: "#111",
-          color: "#fff",
-          padding: 8,
-          fontSize: 12,
-          zIndex: 99999,
-        }}
-      >
-        LINKEDIN_ID: {String(!!process.env.REACT_APP_LINKEDIN_CLIENT_ID)}
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="*" element={<div>404</div>} />
+    </Routes>
   );
 }
