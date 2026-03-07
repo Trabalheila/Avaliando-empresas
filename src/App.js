1ac1a463b3562d1bfc9232042d60b4651a5af9d5
Caio Santana <caio.cad@gmail.com>
2026-03-06 23:08:52 -0300

    fix: atualiza mobile, corrige lista de empresas e resolve tela branca do linkedin

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import Home from './Home';
-
-function CompanyDetailPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-200">
-      <h1 className="text-3xl font-bold text-gray-800">Página de Detalhes da Empresa (Em construção)</h1>
-    </div>
-  );
-}
+import AuthLinkedIn from './AuthLinkedIn'; // <-- Importe aqui
 
 function App() {
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID || ""}>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/empresa/:companyName" element={<CompanyDetailPage />} />
-        </Routes>
-      </div>
-    </GoogleOAuthProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth/linkedin" element={<AuthLinkedIn />} /> {/* <-- Rota que resolve a tela branca */}
+    </Routes>
   );
 }
-
 export default App;
